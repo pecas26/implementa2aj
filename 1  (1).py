df1097602e8984b00b9fcb00f70ab8f5fdec112c
pecas26 <je4178965@gmail.com>
2025-03-21 22:20:31 -0600

    Add files via upload

---
+++ 1  (1).py
@@ -0,0 +1,9 @@
+x=int(input("teclea un numero entero"))
+if x% 2==0:
+	ifx>10:
+	print("este numero es par,pero no mayor a 10")
+	else:
+	print("este numero es par , pero no mayor a 10")
+	else:
+	print ("el numero no es par ")
+	
